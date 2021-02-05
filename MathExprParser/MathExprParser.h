bcc2e651f4aac7bd234f907384c46938b00cec21
Philipp Anné <softyde@annee.de>
2021-02-05 14:27:45 +0100

    Initial Commit

---
+++ MathExprParser/MathExprParser.h
@@ -0,0 +1,18 @@
+//
+//  MathExprParser.h
+//  MathExprParser
+//
+//  Created by Philipp Anné on 05.02.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MathExprParser.
+FOUNDATION_EXPORT double MathExprParserVersionNumber;
+
+//! Project version string for MathExprParser.
+FOUNDATION_EXPORT const unsigned char MathExprParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MathExprParser/PublicHeader.h>
+
+
